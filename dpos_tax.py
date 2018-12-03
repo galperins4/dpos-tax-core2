1ad8aeb35a9a69d792aedaae6381ecce0e586ed9
galperins4 <galperins4@gmail.com>
2018-12-02 21:58:01 -0600

    Update dpos_tax.py

--- dpos_tax.py
+++ dpos_tax.py
@@ -8,7 +8,7 @@ from util.config import use_network
 from crypto.identity.address import address_from_public_key
 
 
-test_acct = [""]
+test_acct = ["032cfbb18f4e49952c6d6475e8adc6d0cba00b81ef6606cc4927b78c6c50558beb"]
 exchange_acct = ["AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK", "AFrPtEmzu6wdVpa2CnRDEKGQQMWgq8nE9V","ARXhacG5MPdT1ehWPTPo8jtfC5NrS29eKS",
                  "AJbmGnDAx9y91MQCDApyaqZhn6fBvYX9iJ","AcVHEfEmFJkgoyuNczpgyxEA3MZ747DRAu","ANQftoXeWoa9ud9q9dd2ZrUpuKinpdejAJ"]
 exceptions = [""]
