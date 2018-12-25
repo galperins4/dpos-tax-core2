eb628043649632af9c8968dd7fea3e499bf66f1d
galperins4 <galperins4@gmail.com>
2018-12-24 21:39:32 -0600

    Update dpos_tax.py

--- dpos_tax.py
+++ dpos_tax.py
@@ -11,7 +11,7 @@ from crypto.configuration.network import set_custom_network
 
 acct = [""]
 exchange_acct = {"ark":["AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK", "AFrPtEmzu6wdVpa2CnRDEKGQQMWgq8nE9V","ARXhacG5MPdT1ehWPTPo8jtfC5NrS29eKS",
-                 "AJbmGnDAx9y91MQCDApyaqZhn6fBvYX9iJ","AcVHEfEmFJkgoyuNczpgyxEA3MZ747DRAu","ANQftoXeWoa9ud9q9dd2ZrUpuKinpdejAJ"]
+                 "AJbmGnDAx9y91MQCDApyaqZhn6fBvYX9iJ","AcVHEfEmFJkgoyuNczpgyxEA3MZ747DRAu","ANQftoXeWoa9ud9q9dd2ZrUpuKinpdejAJ"]}
 exceptions = [""]
 n = None
 taxdb = None
