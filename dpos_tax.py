aafcac4119e722953f94f7f994d9fe523bd92b88
galperins4 <galperins4@gmail.com>
2019-03-06 21:54:15 -0600

    Update dpos_tax.py

--- dpos_tax.py
+++ dpos_tax.py
@@ -205,6 +205,10 @@ def lotting(b,s):
                 else:
                     long_cap_gain += cap_gain
 
+                # update tform
+                tmp = [lot_quantity, network, short_ts(j[1]), short_ts(i[0]), 
+                       (sold_price*(lot_quantity/atomic)), (j[3]*(lot_quantity/atomic)) ,cap_gain]
+                tform.append(tmp)
                 # update lot - zero out and status
                 j[8] -= lot_quantity
                 j[7] = "Lot sold"
