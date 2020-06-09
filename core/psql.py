1c42d24ef3cab1d14a2db6901da58c849b20b005
galperins4 <galperins4@gmail.com>
2020-06-08 20:09:06 -0500

    Update psql.py

--- core/psql.py
+++ core/psql.py
@@ -41,7 +41,7 @@ class DB:
                 for i in universe:
                     for j in i[3]['payments']:
                         if j['recipientId'] == account:
-                            tmp = (i[0], j['amount'], i[2], i[3])
+                            tmp = (i[0], j['amount'], i[1], i[2])
                             print(tmp)
                             quit()
             quit()
