11662380b940a631c948d0280bc56ec4117419ef
galperins4 <galperins4@gmail.com>
2020-06-08 20:12:20 -0500

    Update dpos_multi.py

--- dpos_multi.py
+++ dpos_multi.py
@@ -92,8 +92,7 @@ def buy(acct):
     for i in acct:      
         buys = psql.get_transactions(address_from_public_key(i), s)
         print("buys")
-        print(buys)
-        quit()
+        print(len(buys))
         buys_multi = psql.get_multi_tx(address_from_public_key(i), s, universe)
         print("multis")
         print(len(buys_multi))
