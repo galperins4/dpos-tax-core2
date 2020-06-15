e7f50d3e03fedf19e96f8fc55256121ca8799d4f
galperins4 <galperins4@gmail.com>
2020-06-14 19:55:55 -0500

    Update for multi-payments (#3)
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Create dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update psql.py
    
    * Update dpos_multi.py
    
    * Update psql.py

--- core/psql.py
+++ core/psql.py
@@ -21,11 +21,42 @@ class DB:
                 account}' AND "type" = {0} ORDER BY "timestamp" ASC""")
             else:
                 self.cursor.execute(f"""SELECT "timestamp", "amount", "fee", "recipient_id", "id" FROM transactions WHERE "sender_public_key" = '{
-                account}'ORDER BY "timestamp" ASC""")
+                account}' AND "type" <> {6} ORDER BY "timestamp" ASC""")
             return self.cursor.fetchall()
         except Exception as e:
             print(e)
 
+    def get_all_multi(self):
+        # get all multi-payments universe
+        try:
+            self.cursor.execute(f"""SELECT "timestamp", "fee", "sender_public_key", "asset", "id" FROM transactions WHERE "type" = 6 order by "timestamp" DESC""")
+            return self.cursor.fetchall()
+        except Exception as e:
+            print(e)
+    
+    def get_multi_tx(self, account, side, universe):
+        try:
+            acct_multi=[]
+            if side == "Income":
+                for i in universe:
+                    for j in i[3]['payments']:
+                        if j['recipientId'] == account:
+                            tmp = (i[0], int(j['amount']), i[1], i[2], i[4])
+                            acct_multi.append(tmp)
+            else:
+                # need to figure out how to get fee and divide across transactions
+                for i in universe:
+                    if i[2] == account:
+                        multi_count = len(i[3]['payments'])
+                        fee = int(i[1] / multi_count)
+                        for j in i[3]['payments']:
+                            tmp = (i[0], int(j['amount']), fee, j['recipientId'], i[4])
+                            acct_multi.append(tmp)            
+            return acct_multi
+        except Exception as e:
+            print(e)
+            
+            
     def get_delegates(self):
         try:
             self.cursor.execute(f"""SELECT "address" from wallets WHERE "username" is NOT NULL""")
