da9aacf0fe2a72c29c72958c60173b73427a57eb
galperins4 <galperins4@gmail.com>
2021-11-09 18:34:03 -0600

    Update dpos_tax.py

--- dpos_tax.py
+++ dpos_tax.py
@@ -92,10 +92,12 @@ def get_db_price(ts):
 
 
 def buy(acct):
+    tic_a = time.perf_counter()
     s = "Income"
     buy_agg=[]
     universe = psql.get_all_multi()
     tic_x = time.perf_counter()
+    print(f"Get all multi transactions in {tic_a - tic_x:0.4f} seconds")
     
     for i in acct:      
         buys = psql.get_transactions(address_from_public_key(i), s)
