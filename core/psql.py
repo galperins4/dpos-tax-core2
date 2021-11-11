f210378fefa7a7c48b95e8ee03e137a8dbb87a8a
galperins4 <galperins4@gmail.com>
2021-11-10 18:34:34 -0600

    Update psql.py

--- core/psql.py
+++ core/psql.py
@@ -35,11 +35,15 @@ class DB:
             print(e)
     
    
-    def get_acct_multi(self, account):
+    def get_acct_multi(self, account, side):
         # only grab multi-payments associated with account
         try:
-            self.cursor.execute(f"""SELECT "timestamp", "fee", "sender_public_key", "asset", "id" FROM transactions WHERE asset::jsonb @> '{
-            'payments': ['recipientId':'{account}']}'::jsonb""")
+            if side == "Income":
+                self.cursor.execute("""SELECT "timestamp", "fee", "sender_public_key", "asset", "id" from "transactions" WHERE asset::jsonb @> '{
+                                    "payments": [{"recipientId":"%s"}]}'::jsonb order by "timestamp" DESC;""" % (account))
+            else:
+                cursor.execute(f"""SELECT "timestamp", "fee", "sender_public_key", "asset", "id" FROM transactions WHERE "type" = 6 
+                               AND "sender_public_key" = '{account}' order by "timestamp" DESC""")       
             return self.cursor.fetchall()
         except Exception as e:
             print(e)
