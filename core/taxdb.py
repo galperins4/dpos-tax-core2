79d10bf7813413497a3e400cde42573c23af8dcb
galperins4 <galperins4@gmail.com>
2021-11-14 13:41:36 -0600

    Update taxdb.py

--- core/taxdb.py
+++ core/taxdb.py
@@ -67,9 +67,7 @@ class TaxDB:
     
     def get_match_price(self, ts):
         print(ts)
-        ts_two = str(ts)
-        #self.cursor.execute("""SELECT usd FROM prices WHERE timestamp >= '{ts}' ORDER BY timestamp ASC limit 1""")
-        self.cursor.execute("SELECT * FROM prices WHERE timestamp > '{ts_two}'")
+        self.cursor.execute("""SELECT "usd" FROM prices WHERE "timestamp" >= '{ts}' ORDER BY "timestamp" ASC limit 1""")
         price = self.cursor.fetchall()
         print(len(price))
         print(price)
