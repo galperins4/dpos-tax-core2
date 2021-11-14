dfc4e0ffefd8eaa9e2d96fc218c814e935a91c60
galperins4 <galperins4@gmail.com>
2021-11-14 13:07:56 -0600

    Update dpos_tax.py

--- dpos_tax.py
+++ dpos_tax.py
@@ -151,6 +151,10 @@ def create_buy_records(b, d):
             order_amt = i[1]
             tax_lot = counter+1
             price = get_db_price(ts+n['epoch'])
+            print("Old Price", price)
+            price_two = taxdb.get_match_price(ts+n['epoch'])
+            print("New Price" price_two)
+            quit()
             market_value = round((price * (order_amt/atomic)),2)
             convert_ts = convert_timestamp((ts+n['epoch']))
 
