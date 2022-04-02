09da12bc06f8de660aca1a640070804576640e6b
galperins4 <galperins4@gmail.com>
2022-04-02 12:28:14 -0500

    Update client.py

--- util/client.py
+++ util/client.py
@@ -5,11 +5,6 @@ from pathlib import Path
 class Client:
     def __init__(self):
         self.home = str(Path.home())
-        #net = n.split('_')
-        #coin, network = net[0], net[1]
-    
-        #self.core = self.home+'/.config/'+coin+'-core/'+network
-        #self.tbw = self.home+'/core2_tbw'
         
         
     def get_client(self, api_port, ip="localhost"):
